ae8b925cab18cddb2b23523c1c0155cb4c75ca50
Jinx <murashovofficial@gmail.com>
2026-03-22 12:51:59 +0300

    Added simple server based on crow framework

---
+++ Server/Server.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <crow.h>
+
+int main()
+{
+	crow::SimpleApp app;
+
+	CROW_ROUTE(app, "/")([]() {
+		return "Server is online";
+		});
+
+	app.port(8000).run();
+
+	return 0;
+}
